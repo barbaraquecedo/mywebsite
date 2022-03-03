dca155af6d07e9418bdaa6a0b763f0e01221d887
Barbara Quecedo <barbaraquecedo@gmail.com>
2022-03-03 21:20:29 +0100

    chore: connected to mongodb

--- api/app.js
+++ api/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const logger = require('morgan');
 
-
+/** configurations */
+require('./config/db.config');
 
 const app = express();
 
---
+++ api/config/db.config.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/mywebsite';
+
+mongoose.connect(MONGODB_URI)
+    .then(() => console.info(`Successfully connected to the database ${MONGODB_URI}`))
+    .catch((error) => {
+        console.error(`An error occurred while trying to connect to the database ${MONGODB_URI}`, error);
+        process.exit(0)
+    })
+
+process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+        console.info('Mongoose succesfully disconnected')
+        process.exit(0)
+    })
+})
