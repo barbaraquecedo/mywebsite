6cc17da2c8351cf5296161510a3c87ee5ed4fd19
Barbara Quecedo <barbaraquecedo@gmail.com>
2022-03-22 22:40:01 +0100

    feat: react

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/miscellaneous/navbar";
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+          <Routes>
+          </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/components/miscellaneous/navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link, NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+                <NavLink className="navbar-brand" to="/">Bárbara Quecedo</NavLink>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarText">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <NavLink className="nav-link active" aria-current="page" to="/cv">CV</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/blog">Blog</NavLink>
+                        </li>
+                    </ul>
+                    <span className="navbar-text">
+                        Navbar text with an inline element
+                    </span>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
