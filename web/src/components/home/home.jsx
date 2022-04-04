975788a0b83f0fd757caf5b24855af3f9c6d8c95
Barbara Quecedo <barbaraquecedo@gmail.com>
2022-04-04 17:29:23 +0200

    feat:home

--- web/src/components/home/home.jsx
+++ web/src/components/home/home.jsx
@@ -1,5 +1,6 @@
 import './home.css'
 import image from '../../img/yop.jpeg'
+import podcastbackground from '../../img/podcast.png'
 
 function Home() {
     return (
@@ -9,23 +10,28 @@ function Home() {
                     <h1> Bárbara <br /> Quecedo </h1>
                 </div>
                 <div className='podcast-module'>
-                    <h2> Dime con quién andas, <br /> y te diré quién eres </h2>
-                    <img src='https://assets.wprock.fr/emoji/joypixels/512/1f399.png' alt='micro-emoji' />
+                    <div className='text-module'>
+                        <h2> Dime con quién andas, <br /> y te diré quién eres </h2>
+                    </div>
+                    <a className='btn btn-podcast btn-secondary' target="_blank"> Play</a>
+                    <img src={podcastbackground} alt='micro-emoji' />
                 </div>
             </div>
             <div className='second-row d-flex-start'>
-            <div className='milestones'>
-            <ul>
-                <li>Chocolate</li>
-            </ul>
+                <div className='milestones'>
+                    <ul>
+                        <li>Chocolate</li>
+                    </ul>
+                </div>
+
+
+                <div className='portrait'>
+                    <img className='portrait-gif' src='https://media.giphy.com/media/5xaOcLJUZMX9cKtW6ha/giphy.gif' alt='hi' />
+                    <img src={image} alt='me' />
+                </div>
+
             </div>
-            <div className='portrait'>
-                <img className='portrait-gif' src='https://media.giphy.com/media/5xaOcLJUZMX9cKtW6ha/giphy.gif' alt='hi' />
-                <img src={image} alt='me'/>
-            </div>
-            
-            </div>
-        </div>
+        </div >
     )
 }
 
